99cf00213b169f4de96072b8ca8f2e06fd2d1804
TrexPD <leynad157@gmail.com>
2023-01-19 12:00:43 -0300

    pequena refatoração no código

--- src/Timer++.py
+++ src/Timer++.py
@@ -1,41 +1,34 @@
 from time import sleep
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from rich import box
 
 
 # Função que ler apenas números inteiros!
-def leia_int(msg):
+def leia_int(msg: str) -> int or str:
     while True:
         try:
-            numero: int = int(input(msg))
+            console = Console()
+            numero: int = int(console.input(msg).strip())
         except KeyboardInterrupt:
-            print(
-                '\n\033[1;31mO usuario abartou, preferiu não inserir um valor válido!\033[m'
-            )
+            print('\n[red]O usuário abortou, preferiu não inserir um valor válido![/]')
             break
         except:
-            print(
-                '\033[1;31mHummm... parece que você não digitou um número inteiro válido!\033[m'
-            )
+            print('\n[yellow]Hummm... parece que você não digitou um número inteiro válido![/]')
         else:
             return numero
 
 
-# Perfumaria...
-def caixa_dialogo(dialogo=''):
-    print('--' * len(dialogo))
-    print(f'| \033[1;30m{dialogo.center(53)}\033[m')
-    print('--' * len(dialogo))
-
-
 # Timer que funciona perfeitamente, colocando horas, minutos e segundos!
-def timer():
-    tothoras: int = leia_int('Quantas \033[1;32mhoras\033[m para o "Timer": ')
-    totminutos: int = leia_int('Quantos \033[1;32mminutos\033[m para o "Timer": ')
-    segundos: int = leia_int('Quantos \033[1;32msegundos\033[m para o "Timer": ')
-    lembrete: str = str(
-        input('Digite um \033[1;33mlembrete\033[m para o "Timer": ')
-    ).strip()
-    print('')
-    caixa_dialogo('Contagem regresiva iniciada! ')
+def main() -> str:
+    console = Console()
+    tothoras: int = leia_int('Quantas [green]horas[/] para o [b]Timer[/]: ')
+    totminutos: int = leia_int('Quantos [green]minutos[/] para o [b]Timer[/]: ')
+    segundos: int = leia_int('Quantos [green]segundos[/] para o [b]Timer[/]: ')
+    lembrete: str = str(console.input('Digite um [yellow]lembrete[/] para quando o [b]Timer[/] chegar ao fim: ')).strip()
+
+    print(Table('Contagem regresiva iniciada!', box=box.SIMPLE_HEAD))
 
     # Define a quantidade de horas, minutos e segundos!
     while True:
@@ -50,34 +43,27 @@ def timer():
 
     # Faz a contagem regressiva e verifica se o tempo chegou ao fim, ao chegar um lembrete será mostrado!
     while True:
-        for i in range(segundos, -1, -1):
-            print(
-                '\033[1;31m {:02d}:{:02d}:{:02d}\033[m'.format(
-                    tothoras, totminutos, i
-                ),
-                end='\r',
-            )
+        for contador in range(segundos, -1, -1):
+            print(' {:02d}:{:02d}:{:02d}'.format(tothoras, totminutos, contador), end='\r')
             sleep(1)
         if tothoras >= 1 and totminutos == 0:
             tothoras -= 1
             totminutos = 59
             continue
 
-        elif totminutos >= 1 and i == 0:
+        elif totminutos >= 1 and contador == 0:
             totminutos -= 1
             segundos = 59
             continue
 
-        elif totminutos == 0 and i == 0:
+        elif totminutos == 0 and contador == 0:
             if lembrete == '':
-                print('\033[1;33mO Timer chegou ao fim!\033[m')
+                print('\n\n[yellow]A [b]contagem regresiva[/] chegou ao fim![/]')
                 break
             else:
-                print('')
-                print('')
-                print('\033[1;30mLembrete:\033[m')
-                print(f'\033[1;33m{lembrete}\033[m')
+                print('\n\n[b]Lembrete:[/]')
+                print(f'[yellow]{lembrete.capitalize()}[/]')
                 break
 
 
-timer()
+main()
